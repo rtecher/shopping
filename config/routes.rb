e6378d019038e5e2c405a73b194cf8e9039bb4f3
abdurrehim <abdurrehim.sen@bil.omu.edu.tr>
2018-01-22 20:03:56 +0300

    düzeltt

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :products do
+  resources :products, only: [:index, :show] do
     resources :comments
     resources :sepets, only: [:create, :destroy]
   end
