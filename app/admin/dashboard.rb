09d39c7b5812443c295f68d71a055bc5a5e59a1b
abdurrehim <abdurrehim.sen@bil.omu.edu.tr>
2018-01-28 21:29:14 +0300

    markalar

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,6 +10,10 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
+    if Comment.where(:confirmed == nil)
+        link_to "yeni yorumlar var", admin_comments_path
+    end
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
--- app/controllers/detailed_products_controller.rb
+++ app/controllers/detailed_products_controller.rb
@@ -5,6 +5,8 @@ class DetailedProductsController < ApplicationController
   	elsif params[:minprice]
   		filters = {minprice: params[:minprice], maxprice: params[:maxprice], discounted: params[:discounted], enj: params[:enj], tabl: params[:tabl], pept: params[:pept]}
   		@products = Product.filter(filters).paginate(:page => params[:page], :per_page => 15)
+    elsif params[:trademark]
+      @products = Product.where(trademark: params[:trademark]).paginate(:page => params[:page], :per_page => 15)
   	else
   		@products = Product.paginate(:page => params[:page], :per_page => 15)
   	end        
